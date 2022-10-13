bc24fdcccc0679ac1277f6a822ff4c4add54ae6b
posthak <adler_m@mail.ru>
2022-10-13 19:21:03 +0600

    Based

---
+++ example006_Based/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 4;
+int c = 5;
+int d = 13;
+int e = 1;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
