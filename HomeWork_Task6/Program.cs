e77804831ae3ecaf5a47ab8b83c5263d56b6d9da
posthak <adler_m@mail.ru>
2022-10-15 18:38:28 +0600

    HomeWork Task6

---
+++ HomeWork_Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите число, чтобы проверить четное оно или нет: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int reminder;
+int res = Math.DivRem(a, 2, out reminder);
+
+if (reminder == 0) 
+{
+    Console.Write("Число является четным");
+}
+else
+{
+    Console.Write("Число является нечетным");
+}
+
+
