55fd51ede19c0d21a836b08b85d20f56c81051f0
posthak <adler_m@mail.ru>
2022-10-15 17:45:36 +0600

    Home work task2

---
+++ HomeWork_Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = b;
+int min = a;
+
+if (a > max) 
+{
+    max = a;
+    min = b;
+}
+Console.Write("Максимальное значение: ");
+Console.WriteLine(max);
+Console.Write("Минимальное значение: ");
+Console.WriteLine(min);
+
