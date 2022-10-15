6b67e2feed9e408a385d37fbefced72395d30547
posthak <adler_m@mail.ru>
2022-10-15 18:15:19 +0600

    HoweWok Task4

---
+++ HomeWork_Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = b;
+
+if (a > max) max = a;
+if (c > max) max = c;
+
+Console.Write("Максимальное значение: ");
+Console.WriteLine(max);
+
