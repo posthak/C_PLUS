3528b94033adfa6a587d79ae3f2cb1320389fedb
posthak <adler_m@mail.ru>
2022-10-17 12:25:32 +0600

    exam10MetArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿//             0  1   2   3   4   5   6   7   
+int[] array = {13, 172, 351, 4, 1445, 169, 44, 18666};
+{
+    int n = array.Length;
+    int find = 4;
+
+int index = 0;
+
+while(index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+        }
+    index++;
+}
+}
