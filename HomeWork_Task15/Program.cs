d673fe16e74f7260b4b4aefc824417e6e1de9449
posthak <adler_m@mail.ru>
2022-10-20 00:42:35 +0600

    HWTask15

---
+++ HomeWork_Task15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear(); 
+Console.Write("Введите день недели цифрой: ");
+int s = Convert.ToInt32(Console.ReadLine());
+
+if(s == 6 || s == 7) Console.Write($"{s} -> да");  
+else
+{
+    Console.WriteLine($"{s} -> нет");  
+}
