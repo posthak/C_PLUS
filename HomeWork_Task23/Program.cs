f62033cc774915562874af5406d219034f27b566
posthak <adler_m@mail.ru>
2022-10-24 09:21:54 +0600

    HW task23

---
+++ HomeWork_Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+Console.Write($"{N} -> ");
+while (count <= N)
+{
+    double numPower = Math.Pow(count,3);
+    Console.Write($"{numPower} ");
+    count+=1;
+}
