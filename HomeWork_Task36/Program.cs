24ac2db0a69bd2139b6ac73d35d26c0e2d9d588f
posthak <adler_m@mail.ru>
2022-11-12 20:21:11 +0600

    HomeWork_Task36

---
+++ HomeWork_Task36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+int ArrayLength = new Random().Next(4, 10);
+int[] Collection = new int[ArrayLength];
+int ArrayMin = -10;
+int ArrayMax = 100;
+
+int[] col = fillArray(Collection, ArrayMax, ArrayMin, ArrayLength);
+Console.WriteLine($"[{String.Join(", ", col)}] -> {SumOdd(col, ArrayLength)}");
+
+int[] fillArray(int[] Collection, int max, int min, int len)
+{
+    for (int count = 0; count < len; count++)
+    {
+        Collection[count] = new Random().Next(min, max);
+    }
+    return Collection;
+}
+
+int SumOdd(int[] Collection, int len)
+{
+    int sum = 0;
+    for (int count = 0; count < len; count++)
+    {
+        if (count % 2 != 0) sum += Collection[count];
+    }
+    return sum;
+}
