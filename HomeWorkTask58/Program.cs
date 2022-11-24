55fc479e8cf631dc39c41d8f9ba298a208eab07d
posthak <adler_m@mail.ru>
2022-11-24 09:52:37 +0600

    HomeWorkTask58

--- HomeWorkTask58/Program.cs
+++ HomeWorkTask58/Program.cs
@@ -6,37 +6,31 @@
 // 18 20
 // 15 18
 
-// С ПРОВЕРКОЙ НА СООТВЕТСТВИЕ МАСИВОВ
+// С ПРОВЕРКОЙ НА ВОЗМОЖНОСТЬ УМНОЖЕНИЯ
 Console.Clear();
-int mA = 3;
-int nA = 2;
-int mB = 2;
-int nB = 3;
-
+int row1 = 2;
+int coll1 = 4;
+int row2 = 4;
+int coll2 = 2;
 int min = 1;
 int max = 5;
 
-int[,] collectionA = new int[mA, nA];
+int[,] collectionA = new int[row1, coll1];
 int[,] baseCollectionA = GetCollection(collectionA, min, max);
-
-int[,] collectionB = new int[mB, nB];
+int[,] collectionB = new int[row2, coll2];
 int[,] baseCollectionB = GetCollection(collectionB, min, max);
 
-
-PrintOneCollection(baseCollectionA);
-Console.WriteLine("_____");
-Console.WriteLine("");
-PrintOneCollection(baseCollectionB);
+PrintTwoCollection(baseCollectionA, baseCollectionB);
 Console.WriteLine("");
 
-if (baseCollectionA.GetLength(0) == baseCollectionB.GetLength(1))
+if (coll1 != row2)
 {
-    Console.WriteLine("Результирующая матрица будет:");
-    PrintOneCollection(MultiplyCollection(baseCollectionA, baseCollectionB));
+    Console.WriteLine("Умножение невозможно сделать");
 }
 else
 {
-    Console.WriteLine("Массивы не соответствуют, кол-во строк одного масива не равно кол-ву столбцов другого");
+    Console.WriteLine("Результирующая матрица будет:");
+    PrintOneCollection(MultiplyCollection(baseCollectionA, baseCollectionB));
 }
 
 int[,] GetCollection(int[,] coll, int mn, int mx)
@@ -57,7 +51,36 @@ void PrintOneCollection(int[,] collA)
     {
         for (int j = 0; j < collA.GetLength(1); j++)
         {
-            Console.Write($"{collA[i, j]} ");
+            Console.Write($"{collA[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintTwoCollection(int[,] collA, int[,] collB)
+{
+    int maxLengthRow = collA.GetLength(0) > collB.GetLength(0) ? maxLengthRow = collA.GetLength(0) : maxLengthRow = collB.GetLength(0);
+
+    for (int i = 0; i < maxLengthRow; i++)
+    {
+        for (int j = 0; j < collA.GetLength(1); j++)
+        {
+            if (i < collA.GetLength(0))
+            {
+                Console.Write($"{collA[i, j]}  ");
+            }
+            else
+            {
+                Console.Write($"   ");
+            }
+        }
+        Console.Write("|  ");
+        if (i < collB.GetLength(0))
+        {
+            for (int k = 0; k < collB.GetLength(1); k++)
+            {
+                Console.Write($"{collB[i, k]}  ");
+            }
         }
         Console.WriteLine();
     }
@@ -70,90 +93,11 @@ int[,] MultiplyCollection(int[,] colA, int[,] colB)
     {
         for (int j = 0; j < colB.GetLength(1); j++)
         {
-            int sum = 0;
             for (int k = 0; k < colA.GetLength(1); k++)
             {
-                sum += colA[i, k] * colB[k, j];
+                colRes[i, j] += colA[i, k] * colB[k, j];
             }
-            colRes[i, j] = sum;
         }
     }
     return colRes;
 }
-
-// БЕЗ ПРОВЕРКИ НА СООТВЕТСТВИЕ МАСИВОВ
-
-// Console.Clear();
-// int m = 6;
-// int n = 6;
-// int min = 1;
-// int max = 5;
-
-// int[,] collectionA = new int[m, n];
-// int[,] baseCollectionA = GetCollection(collectionA, min, max);
-
-// int[,] collectionB = new int[m, n];
-// int[,] baseCollectionB = GetCollection(collectionB, min, max);
-
-// PrintTwoCollection(baseCollectionA, baseCollectionB);
-// Console.WriteLine();
-// PrintOneCollection(MultiplyCollection(baseCollectionA, baseCollectionB));
-
-// int[,] GetCollection(int[,] coll, int mn, int mx)
-// {
-//     for (int i = 0; i < coll.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < coll.GetLength(1); j++)
-//         {
-//             coll[i, j] = new Random().Next(mn, mx);
-//         }
-//     }
-//     return coll;
-// }
-
-// void PrintTwoCollection(int[,] collA, int[,] collB)
-// {
-//     for (int i = 0; i < collA.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < collA.GetLength(1); j++)
-//         {
-//             Console.Write($"{collA[i, j]} ");
-//         }
-//         Console.Write("| ");
-//         for (int k = 0; k < collB.GetLength(1); k++)
-//         {
-//             Console.Write($"{collB[i, k]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-
-// void PrintOneCollection(int[,] collA)
-// {
-//     for (int i = 0; i < collA.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < collA.GetLength(1); j++)
-//         {
-//             Console.Write($"{collA[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-
-// int[,] MultiplyCollection(int[,] colA, int[,] colB)
-// {
-//     int[,] colRes = new int[colA.GetLength(0), colA.GetLength(1)];
-//     for (int i = 0; i < colA.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < colB.GetLength(1); j++)
-//         {
-//             int sum = 0;
-//             for (int k = 0; k < colA.GetLength(1); k++)
-//             {
-//                 sum += colA[i, k] * colB[k, j];
-//             }
-//             colRes[i, j] = sum;
-//         }
-//     }
-//     return colRes;
-// }
